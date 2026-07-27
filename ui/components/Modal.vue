50718f6f898604594cf0f1d74886f992865e2989
Kentaro Takasaki <kt0319@gmail.com>
2026-07-27 11:30:56 +0900

    fix: モバイル設定画面でmodal-headerの上マージンによりタブとの隙間ができる不具合を修正

--- ui/components/Modal.vue
+++ ui/components/Modal.vue
@@ -259,7 +259,7 @@ onMounted(() => {
   border-bottom: none;
   border-top: 1px solid var(--border);
   margin-bottom: 0;
-  margin-top: 8px;
+  margin-top: 0;
   padding-bottom: calc(env(safe-area-inset-bottom) + 22px);
 }
 
